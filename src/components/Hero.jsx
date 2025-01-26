f6f50a8b65f06df3206592a3fcb6564bac4dd190
Uyen Pham <uyenbao.pt@gmail.com>
2025-01-26 01:40:51 -0800

    Fix gsap animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,36 +7,37 @@ const Hero = () => {
   const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
   const handleVideoSrcSet = () => {
-    if(window.innerWidth < 760) {
+    if (window.innerWidth < 760) {
       setVideoSrc(smallHeroVideo)
     } else {
       setVideoSrc(heroVideo)
     }
   }
 
-  useEffect(() => {
-    window.addEventListener('resize', handleVideoSrcSet);
+    useEffect(() => {
+      window.addEventListener('resize', handleVideoSrcSet);
 
-    return () => {
-      window.removeEventListener('resize', handleVideoSrcSet)
-    }
-  }, [])
-  
-  useGSAP(() => {
-    gsap.to('#hero', { opacity: 1, delay: 1.5 })
-  }, [])
-  return (
-    <section className = "w-full nav-height bg-black relative">
-      <div className="h-5/6" w-full flex-center flex-col>
-        <p id = "hero" className="hero-title">iPhone 16 Pro</p>
-        <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted playsInline = {true} key = {videoSrc}>
-            <source src = {videoSrc} type = "video.mp4" />
-          </video>
+      return () => {
+        window.removeEventListener('resize', handleVideoSrcSet)
+      }
+    }, [])
+
+    useGSAP(() => {
+      gsap.to('#hero', { opacity: 1, delay: 1.5 })
+    }, [])
+
+    return (
+      <section className="w-full nav-height bg-black relative">
+        <div className="h-5/6" w-full flex-center flex-col>
+          <p id="hero" className="hero-title">iPhone 16 Pro</p>
+          <div className="md:w-10/12 w-9/12">
+            <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+              <source src={videoSrc} type="video/mp4" />
+            </video>
+          </div>
         </div>
-      </div>
-    </section>
-  )
-}
+      </section>
+    )
+  }
 
 export default Hero
