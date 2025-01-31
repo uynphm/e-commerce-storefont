4cdd9b89ed2462e745b14bc797bb52f973f4d64b
Uyen Pham <uyenbao.pt@gmail.com>
2025-01-30 22:15:22 -0800

    add VideoCarousel and animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    <header clasName="w-full py-5 sm:px-10 px-5 flex justify-between items center">
+    <header clasName="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt = "Apple" with = {14} height = {18} />
 
